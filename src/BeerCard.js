605ce6abceb49471e672acf33f2d93dbee25014f
HollyLgru <H.grudovich@zollege.com>
2022-07-25 08:37:23 -0500

    added button

--- src/BeerCard.js
+++ src/BeerCard.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import "./beercard.css";
 
 
 
+
 function BeerCard(props) {
+const [likes, setLikes] = useState(0);
+const [isClicked, setIsClicked] = useState(false);
+
+const handleClick = () => {
+    if (isClicked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+    setIsClicked(!isClicked);
+};
+
     return(
-        <li>
-            <img src={props.image_url} alt=""></img>
-            <h3> 
+    <li>
+        <img src={props.image_url} alt=""></img>
+        <h3> 
             {props.name} <span> {props.first_brewed}</span> 
-    </h3>
-  <h4>{props.tagline}</h4>
-   <p>
-     <span>{props.abv}</span>
-     {props.description}
-  </p>
-        </li>
+        </h3>
+        <h4>{props.tagline}</h4>
+        <p>
+            <span>{props.abv}</span>
+            {props.description}
+        </p>
+        <br></br>
+        <button className={ `like-button ${isClicked && 'liked'}` } onClick={ handleClick }>
+            <span className="likes-counter">{ `Like | ${likes}` }</span>
+        </button>
+    </li>
     )
 };
 
